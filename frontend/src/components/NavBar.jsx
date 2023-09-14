c5e2e8ebc20a25ca4994479616a66c590d3f34ce
Daniel Jeng <dbjeng@gmail.com>
2023-09-13 22:11:25 -0500

    Edited Home page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import {Container, Nav, Navbar as ReactNavbar} from 'react-bootstrap';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
     <ReactNavbar bg="primary" data-bs-theme="dark">
-      <Container>
+      <Container className={styles.container}>
         <ReactNavbar.Brand href="/">Guitar for the Church Life</ReactNavbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/keyChordTable">Key-Chord Table</Nav.Link>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import styles from './Home.module.css';
+
 export default function Home(){
     return (
-        <div>Home Page</div>
+        <div className={styles.verse}>
+            <div>
+                <div className={styles.textLine}>"I will sing to Jehovah while I live; /</div>
+                <div className={styles.textLine}>I will sing psalms to my God while I yet have being."</div>
+                <div className={styles.textLine}>Psa. 104:33</div>
+            </div>
+        </div>
     )
 }
