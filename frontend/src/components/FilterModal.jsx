a0dcd6dd0c66a64e1ab224ab18827977776bd877
Daniel Jeng <dbjeng@gmail.com>
2024-05-16 13:39:33 -0600

    All frontend changes

--- frontend/src/components/FilterModal.jsx
+++ frontend/src/components/FilterModal.jsx
@@ -5,9 +5,10 @@ import { useEffect } from 'react';
 import styles from "./FilterModal.module.css";
 
 export default function FilterModal({showModal, onHide, allowedChords, onSave}) {
-  console.log('allowedChords:', allowedChords)
+  const MOST_COMMON_CHORDS = new Set("G", "D", "A", "Bm", "A7", "Em", "F#m", 
+    "C", "D7", "Am", "F", "G7", "Dm", "E", "E7", "C7", "B7", "F#", "Am7",
+    "Gm", "B", "Em7", "Dm7", "Bb", "Cmaj7", "F#7", "Cm", "Dmaj7", "Bm7", "C#m", "Fm")
   const [checkedChords, setCheckedChords] = useState(allowedChords);
-  console.log("check initialization: ", checkedChords)
   
   // Update checkedChords when allowedChords changes
   useEffect(() => {
@@ -23,12 +24,18 @@ export default function FilterModal({showModal, onHide, allowedChords, onSave}) 
     });
   }
 
-  function generateCheckboxes() {
+  function generateCheckboxes(commonChords) {
     return Object.keys(checkedChords).map((chord) =>
-      <div key={chord}>
-        <input type="checkbox" id={chord} name={chord} checked={checkedChords[chord]} onChange={handleCheckboxChange}/>
-        <label for={chord}>{chord}</label>
-      </div>
+      {
+        if (commonChords && chord in MOST_COMMON_CHORDS || !commonChords && !(chord in MOST_COMMON_CHORDS)) {
+          return (
+            <div key={chord}>
+              <input type="checkbox" id={chord} name={chord} checked={checkedChords[chord]} onChange={handleCheckboxChange}/>
+              <label for={chord}>{chord}</label>
+            </div>
+          )
+        }
+      }
     )
   }
 
@@ -40,8 +47,11 @@ export default function FilterModal({showModal, onHide, allowedChords, onSave}) 
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className="grid-example">
-        <div className={styles.allowedChordsContainer}>          
-          {generateCheckboxes()}
+        <div className={styles.allowedChordsContainer}>     
+          {generateCheckboxes(true)}
+        </div>        
+        <div className={styles.allowedChordsContainer}>     
+          {generateCheckboxes(false)}
         </div>
       </Modal.Body>
       <Modal.Footer className={styles.footer}>
